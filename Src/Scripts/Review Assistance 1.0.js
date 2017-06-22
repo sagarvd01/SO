342331c4e62796e32be94ead505ae1b3ada416c7
sagarvd01 <sagarvd01@users.noreply.github.com>
2017-06-22 12:58:10 +0530

    Add files via upload

---
+++ Src/Scripts/Review Assistance 1.0.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Review Assistance 1.0
+// @namespace    Sagar V
+// @version      1.0
+// @description  Assist to complete the Review Task
+// @author       Sagar V
+// @match        *.stackoverflow.com/review/close*
+// @match        *.stackoverflow.com/review/reopen*
+// @grant        none
+// @website      https://sagarvd01.github.io/
+// @run-at       document-end
+// ==/UserScript==
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(r(){\'11 1a\';7 t=o.5(".1f-1c-19");7 k=o.C("a");k.10.Z="1e";k.9="1b";7 e=F K();e.E("D","1d://18.15/q/13/14",l);e.G=r(){3(e.J==4&&e.s==u){16.12("17")}};e.A();k.1h(\'1o\',I);t.1p.1q(k,t);r I(){7 n=o.5(".B-1r");7 6=F K();6.E("D",n,l);6.G=r(){3(6.s==u&&6.J==4){7 p=o.C(\'p\');p.9=6.1i;7 2=p.5(".x-B-n");3(y.L.H.O("/x/")!=-1){3(2.c("b-f")=="v"&&2.5("d.i-g-h")&&W(2.5("d.i-g-h").9)>0){8(2.5("d.i-g-h").9+"-z m\\U 1j")}j 3(2.c("b-f")=="v"&&2.V.N===0){8("M z m")}j 3(2.c("b-f")=="l"){8("R w S T.\\1g Q. Y X")}}j 3(y.L.H.O("1k")!=-1){3(2.c("b-f")=="l"&&2.5("d.i-g-h")&&W(2.5("d.i-g-h").9)>0){8(2.5("d.i-g-h").9+"-P m\\U 1m")}j 3(2.c("b-f")=="l"&&2.V.N===0){8("M P m")}j 3(2.c("b-f")=="v"){8("R w S T.\\1l Q. Y X")}}}3(6.s!=u){8("n 1n 1s")}};6.A()}})();',62,91,'||element|if||querySelector|xhr|var|alert|innerHTML||data|getAttribute|span|xh|isclosed|flag|count|existing|else|elm|true|Votes|link|document|||function|status|place|200|false|is|close|window|Close|send|question|createElement|GET|open|new|onreadystatechange|href|checkStatus|readyState|XMLHttpRequest|location|No|length|indexOf|Reopen|Question|This|an|Audit|nNot|children|parseInt|Carefull|Be|textDecoration|style|use|log|44481131|2427065|com|console|Initialized|stackoverflow|toggle|strict|Status|summary|https|none|review|nClosed|addEventListener|responseText|Closed|reopen|nOpen|Reopened|not|click|parentNode|insertBefore|hyperlink|found'.split('|'),0,{}))
